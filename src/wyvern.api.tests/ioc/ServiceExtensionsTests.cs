896ea1efe67ab377f3f1ee62808aeefa39d98e4b
Jonathan Nagy <nagytech@users.noreply.github.com>
2019-04-26 16:44:13 +1000

    Api endpoint tests.

--- src/wyvern.api.tests/ioc/ServiceExtensionsTests.cs
+++ src/wyvern.api.tests/ioc/ServiceExtensionsTests.cs
@@ -30,6 +30,83 @@ namespace wyvern.api.tests.ioc
         public string Name { get; set; }
     }
 
+    public class Tests : IClassFixture<TestApiFactory<TestStartup>>
+    {
+        private WebApplicationFactory<TestStartup> AppFactory { get; }
 
+        public Tests(TestApiFactory<TestStartup> appFactory)
+        {
+            AppFactory = appFactory;
+        }
+
+        [Theory]
+        [InlineData("/swagger/v1/swagger.json")]
+        public async Task api_provides_swagger_generation(string url)
+        {
+            var client = AppFactory.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var str = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(str);
+        }
+
+        [Theory]
+        [InlineData("/api/user/test")]
+        [InlineData("/api/user/test/friend/test2")]
+        public async Task api_accepts_get_requests(string url)
+        {
+            var client = AppFactory.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var str = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(str);
+        }
+
+        [Theory]
+        [InlineData("/api/users")]
+        [InlineData("/api/user/test/friends")]
+        public async Task api_accepts_post_requests(string url)
+        {
+            var client = AppFactory.CreateClient();
+            var content = new StringContent("{\"name\": \"test_name\"}");
+            var response = await client.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+            var str = await response.Content.ReadAsStringAsync();
+        }
+
+        [Theory]
+        [InlineData("/api/user/test/friend-seq/1")]
+        [InlineData("/api/user/test/friend-seq/400")]
+        public async Task supports_integer_route_constraints(string url)
+        {
+            var client = AppFactory.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var str = await response.Content.ReadAsStringAsync();
+        }
+
+        [Theory]
+        [InlineData("/api/user/test/friend-seq/1-f00")]
+        [InlineData("/api/user/test/friend-seq/baz")]
+        public async Task integer_route_constraints_must_not_be_strings(string url)
+        {
+            var client = AppFactory.CreateClient();
+            var response = await client.GetAsync(url);
+            Assert.Equal(404, (int)response.StatusCode);
+        }
+
+
+        [Theory]
+        [InlineData("/api/not-a-real-route")]
+        [InlineData("/api/something-they-would-not-use")]
+        public async Task unknown_routes_return_404(string url)
+        {
+            var client = AppFactory.CreateClient();
+            var response = await client.GetAsync(url);
+            Assert.Equal(404, (int)response.StatusCode);
+        }
+
+
+    }
 
 }

5084f4f2379b0ac80785fae1ab96412b9870f6fa
Jonathan Nagy <nagytech@users.noreply.github.com>
2019-09-02 11:13:09 +1000

    Adjust startup to use actor lifetime

--- src/wyvern.api/ioc2/ReactiveServicesStartup.cs
+++ src/wyvern.api/ioc2/ReactiveServicesStartup.cs
@@ -1,16 +1,7 @@
-using System.IO;
 using Akka.Actor;
-using Akka.Bootstrap.Docker;
-using Akka.Event;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using wyvern.api;
-using wyvern.api.abstractions;
-using wyvern.api.@internal.readside;
-using wyvern.api.@internal.sharding;
-using wyvern.bootstrap.Docker;
-using static wyvern.api.@internal.readside.ClusterDistributionExtensionProvider;
 
 public class ReactiveServicesStartup
 {
@@ -25,9 +16,12 @@ public class ReactiveServicesStartup
     /// <param name="services"></param>
     public void ConfigureServices(IServiceCollection services)
     {
-        services.ConfigureReactiveServicesApi();
-        services.AddActorSystem()
-                .AddReactiveComponents();
+        // TODO: This isn't as nice as I would like it to be, revisit soon...
+        services.AddSingleton<ActorSystemLifetime>();
+        services.AddSingleton<ActorSystem>(x => {
+            var actorSystem = x.GetService<ActorSystemLifetime>().CreateActorSystem();
+            return actorSystem;
+        });
     }
 
     /// <summary>
@@ -37,9 +31,9 @@ public class ReactiveServicesStartup
     /// <param name="env"></param>
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
+        app.ApplicationServices.GetService<ActorSystemLifetime>();
         app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
         app.UseWebSockets();
-        app.UseReactiveServicesRouter();
 
         // TODO: register streams
         // TODO: services by reflection
